29d3e2e7c2f5a8c187eb38abb1282faf75d3aae2
Håkon <haakonu97@gmail.com>
2022-07-04 10:49:29 +0200

    chore: More work on nextjs setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,28 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout";
 import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+import { useMountedEffect } from "../hooks/useMountedEffect";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useMountedEffect(() => {
+    /** Helps with migration from old react hashrouter */
+    if (window.location.href.includes("/#/")) {
+      router.replace(window.location.href.replace("/#/", "/"));
+    }
+  }, [router.asPath]);
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
     </Layout>
   );
 }
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion";
 import { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import styled from "styled-components";
+import { Button } from "../components/Button";
+import { applyFontKind } from "../styled-utils";
 import { ArticleItem } from "../types/wordpress";
 
 type PostsResponse = Array<ArticleItem>;
@@ -9,23 +12,72 @@ const ArticlesPage: NextPage<{
   posts: PostsResponse;
 }> = ({ posts }) => {
   return (
-    <motion.div>
+    <Container
+      initial={{
+        transform: "translateY(-20px)",
+        opacity: 0,
+      }}
+      animate={{
+        transform: "translateY(0%)",
+        opacity: 1,
+      }}
+      exit={{
+        transform: "translateY(20px)",
+        opacity: 0,
+      }}
+      transition={{
+        ease: "anticipate",
+        duration: 0.8,
+      }}
+      key="articles-page"
+    >
+      <h1>Articles</h1>
       <ul>
-        {posts.map((post) => (
-          <li>
-            <Link href={`/article/${post.slug}`} passHref>
-              <a
-                dangerouslySetInnerHTML={{
-                  __html: post.title.rendered,
-                }}
-              />
-            </Link>
-          </li>
-        ))}
+        {posts
+          .filter(
+            (item) =>
+              item.categories.includes(2) ||
+              item.categories.includes(3)
+          )
+          .map((post) => (
+            <li>
+              <Link
+                href={`/article/${post.slug}`}
+                passHref
+                scroll={false}
+              >
+                <a
+                  dangerouslySetInnerHTML={{
+                    __html: post.title.rendered,
+                  }}
+                />
+              </Link>
+            </li>
+          ))}
       </ul>
-    </motion.div>
+    </Container>
   );
 };
+
+const Container = styled(motion.div)`
+  max-width: 900px;
+  margin: 0 auto;
+  h1 {
+    ${applyFontKind("h1")}
+  }
+  ul {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    a {
+      display: block;
+      ${applyFontKind("button")}
+      padding:var(--s-03);
+    }
+  }
+`;
 
 export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch(
