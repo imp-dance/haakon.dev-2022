d7b90cff75b8b98626bd2f7d1e04ba7a7b533798
Håkon <haakonu97@gmail.com>
2022-07-31 22:04:24 +0200

    chore: Removed prisma in favour of Supabase

--- pages/api/generate_link.tsx
+++ pages/api/generate_link.tsx
@@ -1,53 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient, User } from "@prisma/client";
-import {
-  PrismaClientInitializationError,
-  PrismaClientKnownRequestError,
-  PrismaClientRustPanicError,
-  PrismaClientValidationError,
-} from "@prisma/client/runtime";
+import { supabase } from "lib/initSupabase";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data =
   | {
-      user: User | null;
+      data: any | null;
     }
   | {
       error: any;
     };
-
-const prisma = new PrismaClient();
-
-type PrismaError =
-  | PrismaClientInitializationError
-  | PrismaClientValidationError
-  | PrismaClientKnownRequestError
-  | PrismaClientRustPanicError;
-
-const isPrismaError = (err: unknown): err is PrismaError => {
-  return (
-    err instanceof PrismaClientInitializationError ||
-    err instanceof PrismaClientValidationError ||
-    err instanceof PrismaClientKnownRequestError ||
-    err instanceof PrismaClientRustPanicError
-  );
-};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   try {
-    const params = new URLSearchParams(
-      req.url?.split?.("?")[1] ?? ""
-    );
-    const user = await prisma.user.findFirst();
-    res.status(200).json({ user });
+    const { data, error } = await supabase
+      .from("links")
+      .select("*");
+    if (error) {
+      return res.status(500).json({
+        error,
+      });
+    }
+    return res.status(200).json({
+      data,
+    });
   } catch (err) {
-    if (isPrismaError(err))
-      return res.status(500).json({ error: err.message });
-    return res
-      .status(500)
-      .json({ error: { text: "Unknown error", data: err } });
+    return res.status(500).json({
+      error: err ?? null,
+    });
   }
 }
