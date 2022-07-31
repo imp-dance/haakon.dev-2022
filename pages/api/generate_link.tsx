603ef172f5ca5dfae7790e3a9ab9bdaf2ce719a7
Håkon <haakonu97@gmail.com>
2022-07-31 19:10:51 +0200

    chore: Testing error handling

--- pages/api/generate_link.tsx
+++ pages/api/generate_link.tsx
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient, User } from "@prisma/client";
+import { PrismaClientValidationError } from "@prisma/client/runtime";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data =
@@ -22,7 +23,9 @@ export default async function handler(
     );
     const user = await prisma.user.findFirst();
     res.status(200).json({ user });
-  } catch (error: any) {
-    res.status(500).json({ error });
+  } catch (err) {
+    if (err instanceof PrismaClientValidationError)
+      return res.status(500).json({ error: err.message }); // sending the error message directly
+    return res.status(500).json({ error: "unknown error" });
   }
 }
