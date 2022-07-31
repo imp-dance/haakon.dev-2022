93a36384bea7bf8809ccd10c16efc52e33465805
Håkon <haakonu97@gmail.com>
2022-07-31 19:18:12 +0200

    chore: Better error handling

--- pages/api/generate_link.tsx
+++ pages/api/generate_link.tsx
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient, User } from "@prisma/client";
-import { PrismaClientValidationError } from "@prisma/client/runtime";
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data =
@@ -13,6 +18,21 @@ type Data =
 
 const prisma = new PrismaClient();
 
+type PrismaError =
+  | PrismaClientInitializationError
+  | PrismaClientValidationError
+  | PrismaClientKnownRequestError
+  | PrismaClientRustPanicError;
+
+const isPrismaError = (err: unknown): err is PrismaError => {
+  return (
+    err instanceof PrismaClientInitializationError ||
+    err instanceof PrismaClientValidationError ||
+    err instanceof PrismaClientKnownRequestError ||
+    err instanceof PrismaClientRustPanicError
+  );
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -24,8 +44,10 @@ export default async function handler(
     const user = await prisma.user.findFirst();
     res.status(200).json({ user });
   } catch (err) {
-    if (err instanceof PrismaClientValidationError)
-      return res.status(500).json({ error: err.message }); // sending the error message directly
-    return res.status(500).json({ error: "unknown error" });
+    if (isPrismaError(err))
+      return res.status(500).json({ error: err.message });
+    return res
+      .status(500)
+      .json({ error: { text: "Unknown error", data: err } });
   }
 }
